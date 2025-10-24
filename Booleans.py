8ac34e6eb1f61573427d7eb4f408693be4b25d55
IT <jaydeep@skino.local>
2025-10-24 13:53:21 +0530

    24 - 10 - 2025

---
+++ Booleans.py
@@ -0,0 +1,20 @@
+print (10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 10
+b = 20 
+if a > b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+# Filled bool casting returns true 
+c = bool(123)
+print(c)
+
+# blank or 0 bool casting retruns false
+d = bool(0)
+e = bool()
+print(d)
+print(e)
