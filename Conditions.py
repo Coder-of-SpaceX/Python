ef5044da13777279b4d841d67a9ebc49d15565b4
IT <jaydeep@skino.local>
2025-10-25 17:42:40 +0530

    25 - 10 - 2025

---
+++ Conditions.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+if a < b :
+    print("Yes 10 is Smaller")
+
+c = 20
+d = 40
+if c < d :
+    print("D is Greater")
+else:
+    print("c is greater")
+
+e = 0
+if e > 0 :
+    print("The Number specified is a positive number")
+elif e < 0:
+    print("The number specified is a negative number")
+else :
+    print("The number is a 0, nor -ve nor +ve")
+
