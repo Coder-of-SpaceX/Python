d647a0b01540c1a46b7cf30c545aadd28a43476f
IT <jaydeep@skino.local>
2025-10-26 13:21:12 +0530

    26 - 10 - 2025

--- Conditions.py
+++ Conditions.py
@@ -1,20 +1,107 @@
-a = 10
-b = 20
-if a < b :
-    print("Yes 10 is Smaller")
+# Normal Syntax for if condition 
+a = 10 
+b = 30 
+if b > a :
+    print("b is greater")
 
-c = 20
-d = 40
-if c < d :
-    print("D is Greater")
+# Check if a number is +ve or -ve
+
+number = 15
+if number > 0:
+    print(f"{number} is a positive number : - )")
+
+
+# Mulitple print statements in if block.
+age = 20
+if age >= 18:
+    print("You are an adult")
+    print("Apply for learning license")
+    print("after getting learning license you can apply for  the main exam within 6 months")
+    print("after passing the exam as well as receiving the license you are eligible for driving")
+
+# Using variables in if condition 
+is_logged_in = True
+if is_logged_in:
+    print("Welcome Back Buddy !!")
+
+# The Elif keyword 
+c = 33
+d = 33
+if c > d :
+    print("C is greater")
+elif c == d :
+    print("C and D both are equal to each other.")
+
+# Mulitple Elif statements
+
+score = 51
+
+if score >=90 :
+    print("Grade A")
+elif score >=80 :
+    print("Grade B")
+elif score >=70:
+    print("Grade C")
+elif score >=60:
+    print("Grade D")
+elif score >=50:
+    print("Grade E")
+elif score <=40 | score >= 40:
+    print("No Grade, Poor Student") 
+
+# Note "elif condition" works in such a way that when mulitple conditions are declared using elif
+# if one of the conditions get true so than the compilor skips the other conditions to check.
+
+age = 13
+if age <=13:
+    print("You are a child")
+if age < 20:
+    print("You are a teenager")
+if age < 30:
+    print("You are an adult now")
+if age > 35:
+    print("you are a married man now as well as a bread earner")
+if age > 60:
+    print("You come under senior citizen section")    
+
+# day = int(input("Enter the number of a day and not days -  "))
+# print(day)
+
+# if day == 1:
+#     print("Sunday")
+# elif day == 2:
+#     print("Monday")
+# elif day == 3:
+#     print("Tuesday")
+# elif day == 4:
+#     print("Wednesday")
+# elif day == 5:
+#     print("Thursday")
+# elif day == 6:
+#     print("Friday")
+# elif day == 7:
+#     print("Saturday")
+
+# The Else Keyword
+
+a = 20
+b = 30
+
+if a > b : 
+    print("a is greater")
+elif a == b:
+    print("both are equal")
 else:
-    print("c is greater")
+    print("b is greater")
 
-e = 0
-if e > 0 :
-    print("The Number specified is a positive number")
-elif e < 0:
-    print("The number specified is a negative number")
-else :
-    print("The number is a 0, nor -ve nor +ve")
+# Else Without Elif
+a =200
+b = 40
+if b > 1:
+    print("b is greater")
+else:
+    print("A is greater")
+# NOTE - after else no other statment can be used like if or elif else is the final statement.
 
+# Checking even or odd 
+number = 7
